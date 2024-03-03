86c7403429bbac68430ba07670db7c171870ff1a
LevaniVashadze <vashadzelevani11@gmail.com>
2024-03-03 12:56:08 +0400

    small fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ import About from "./components/About";
 import Work from "./components/Work";
 import Contact from "./components/Contact";
 import ProgressBar from "./components/ProgressBar.jsx";
-import { Suspense } from "react";
+import { Suspense, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import Footer from "./components/Footer.jsx";
 
 function App() {
   const { t } = useTranslation();
+
+  useEffect(() => {
+    window.HSStaticMethods.autoInit();
+  }, []);
   return (
     <div className="App scroll-smooth font-oswald">
       <Suspense fallback={<div>{t("loading")}</div>}>
